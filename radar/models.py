a6a2442c24ee223165429ef81a8569383f18e0ef
Yevhenii Diemientiev <yevhenii.diemientiev.pe@gmail.com>
2023-09-15 13:43:12 +0300

    Add InstagramAccount model

--- radar/models.py
+++ radar/models.py
@@ -1,19 +1,20 @@
-from django.db import models
-
 import requests
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 from rest_framework.exceptions import AuthenticationFailed
 
 from logging_conf import logger
 from radar.utils import get_facebook_accounts_url
 
 
-class UserManager(models.Manager):
+class InstagramAccountManager(models.Manager):
     @staticmethod
-    def get_user_ig_id(ig_token: str) -> str:
+    def get_account_id(ig_token: str) -> str:
         instagram_account_url = get_facebook_accounts_url(ig_token)
         response = requests.get(instagram_account_url)
         if response.status_code != 200:
-            logger.error(f"User instagram account receiving failed. {response.content=}")
+            logger.error(f"Instagram account receiving failed. {response.content=}")
             raise AuthenticationFailed()
 
         id = response.json().get('data')[0].get('instagram_business_account').get('id')
@@ -21,18 +22,29 @@ class UserManager(models.Manager):
 
     def get_or_create_with_ig_token(self, ig_token: str):
         ig_id = self.get_user_ig_id(ig_token)
-        user, created = super().get_or_create(ig_id = ig_id)
-        user.ig_token = ig_token
-        user.save()
-        return user, created
+        ig_account, created = super().get_or_create(id = ig_id)
+        ig_account.ig_token = ig_token
+        ig_account.save()
+        return ig_account, created
 
 
-class User(models.Model):
-    ig_id = models.CharField(unique=True)
-    ig_token = models.CharField()
-    last_modified = models.DateTimeField(auto_now=True)
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
 
-    objects = UserManager()
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
-        return f"User {self.ig_id}, modified {self.last_modified}"
+        return f"User {self.id}, {self.email}"
+
+
+class InstagramAccount(models.Model):
+    id = models.CharField(primary_key=True)
+    access_token = models.CharField()
+    last_login = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name="ig_accounts")
+
+    objects = InstagramAccountManager()
+
+    def __str__(self):
+        return f"IG account {self.id}, last accessed {self.last_login}"
