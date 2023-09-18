5b554df938e5d87b1d03ad24a4a946e813739a34
Yevhenii Diemientiev <yevhenii.diemientiev.pe@gmail.com>
2023-09-18 10:31:38 +0300

    Refactoring

--- radar/views/login.py
+++ radar/views/login.py
@@ -1,5 +1,5 @@
 import requests
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.views import View
 from rest_framework.exceptions import AuthenticationFailed
 
@@ -11,19 +11,6 @@ from radar.utils import get_facebook_oauth_url, get_facebook_token_url
 # Create your views here.
 
 class AuthorizationView(View):
-    def get(self, request):
-        if 'code' in request.GET:
-            return self.authorize_user(request)
-        else:
-            return self.send_facebook_login_url()
-
-    def authorize_user(self, request):
-        authorization_code = request.GET.get('code')
-        access_token = self.get_facebook_access_token(authorization_code)
-        user, created = User.objects.get_or_create_with_ig_token(access_token)
-        logger.error(f"Login completed. {user=}, {created=}")
-        return HttpResponse(status = 201)
-
     @staticmethod
     def send_facebook_login_url():
         facebook_login_url = get_facebook_oauth_url()
@@ -39,3 +26,20 @@ class AuthorizationView(View):
             raise AuthenticationFailed()
 
         return response.json().get('access_token')
+
+    def get(self, request):
+        if 'code' in request.GET:
+            user = self.__authenticate_user(request)
+            return self.__authorize_user(user)
+        else:
+            return self.send_facebook_login_url()
+
+    def __authenticate_user(self, request):
+        authorization_code = request.GET.get('code')
+        access_token = self.get_facebook_access_token(authorization_code)
+        user, created = User.objects.get_or_create_with_ig_token(access_token)
+        logger.error(f"Login completed. {user=}, {created=}")
+        return user
+
+    def __authorize_user(self, user):
+        pass
