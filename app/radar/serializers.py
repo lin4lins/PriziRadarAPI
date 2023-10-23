f9ab249f02a5b12d1708ae8bcf79d357e27c2a64
Elina Shramko <elina.vl.shramko@gmail.com>
2023-10-23 15:10:23 +0300

    Add serializers for new models

--- app/radar/serializers.py
+++ app/radar/serializers.py
@@ -1,54 +1,26 @@
 from rest_framework import serializers
-from radar.models import InstagramAccount, InstagramPost, User, InstagramComment
+
+from radar.models import Account, Connection
 
 
-class UserSerializer(serializers.ModelSerializer):
+class AccountSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = User
+        model = Account
         fields = '__all__'
-        extra_kwargs = {'password': {'write_only': True}}
+        read_only = ['name', 'username', 'avatar_url']
+
+
+class ConnectionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Connection
+        fields = ['id', 'ig_token']
+        write_only = ['ig_token']
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = super().create(validated_data)
-        user.set_password(password)
-        user.save()
-        return user
-
-
-class InstagramAccountSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(read_only=True)
-
-    class Meta:
-        model = InstagramAccount
-        fields = '__all__'
-        read_only_fields = ["ig_id", "user"]
-
-
-class InstagramPostSerializer(serializers.ModelSerializer):
-    account_id = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = InstagramPost
-        exclude = ['ig_account']
-        read_only_fields = ['ig_id', 'shortcode', 'ig_account']
-
-    def get_or_create(self, validated_data):
-        url = validated_data.get('url')
-        existing_post = InstagramPost.objects.filter(url=url).first()
-        if existing_post:
-            return existing_post, False
-
-        account_id = validated_data.pop('account_id')
-        ig_account = InstagramAccount.objects.get(id=account_id)
-        validated_data['ig_account'] = ig_account
-        return super().create(validated_data), True
-
-
-class InstagramCommentSerializer(serializers.ModelSerializer):
-    ig_post = serializers.PrimaryKeyRelatedField(queryset=InstagramPost.objects.all())
-
-    class Meta:
-        model = InstagramComment
-        exclude = ['id']
+        ig_token = validated_data.get('ig_token')
+        account, is_created = Account.objects.get_or_create(ig_token = ig_token)
+        connection = Connection(account = account, **validated_data)
+        connection.save()
+        return connection
