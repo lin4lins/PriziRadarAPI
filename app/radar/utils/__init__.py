e349d1603ebe64b2aca85fbb2bbb0ea1a3e8195d
Elina Shramko <elina.vl.shramko@gmail.com>
2023-10-31 12:15:47 +0200

    Return 401 response if facebook return an OAuthException

--- app/radar/utils/__init__.py
+++ app/radar/utils/__init__.py
@@ -1,10 +1,13 @@
 import requests
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import AuthenticationFailed
 
 GRAPH_API_VERSION = "v17.0"
 FACEBOOK_API_BASE_URL = f"https://graph.facebook.com/{GRAPH_API_VERSION}"
 ACCOUNTS_URL = f"{FACEBOOK_API_BASE_URL}/me/accounts"
 QUERY_HASH = "eaffee8f3c9c089c9904a5915a898814"
+
+
+ERROR_TYPES = {"OAuthException": AuthenticationFailed()}
 
 
 def build_url(base_url, params):
@@ -18,7 +21,9 @@ def make_request(url):
     """Make an HTTP GET request and return the JSON response."""
     response = requests.get(url)
     if response.status_code != 200:
-        raise NotFound(response.json())
+        error_type = response.json()['error']['type']
+        raise ERROR_TYPES[error_type]
+
     return response.json()
 
 
